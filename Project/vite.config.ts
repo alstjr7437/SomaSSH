a09ae38e8087dec14a90fa1524dd96e25eb476e7
Hong <dbsghdz1@gmail.com>
2026-07-11 17:44:02 +0900

    feat: notion 연결

--- Project/vite.config.ts
+++ Project/vite.config.ts
@@ -1,7 +1,20 @@
 import { defineConfig } from 'vite'
 import react from '@vitejs/plugin-react'
+import { handleClaimDocuments, loadLocalEnv } from './server/notionDocuments.ts'
+
+loadLocalEnv()
 
 // https://vite.dev/config/
 export default defineConfig({
-  plugins: [react()],
+  plugins: [
+    react(),
+    {
+      name: 'claim-documents-api',
+      configureServer(server) {
+        server.middlewares.use('/api/claim-documents', (req, res) => {
+          void handleClaimDocuments(req, res)
+        })
+      },
+    },
+  ],
 })
